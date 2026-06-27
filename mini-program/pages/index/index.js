f4b8e5377fc5420a70d64abe3a7b542ea2ede222
YouWeiBot <youwei@mini-web>
2026-06-27 23:33:34 +0800

    feat(mini-program): index page (entry + hasResume check)

---
+++ mini-program/pages/index/index.js
@@ -0,0 +1,27 @@
+const { request } = require('../../utils/request');
+
+Page({
+  data: { hasResume: false },
+
+  onShow() {
+    this.checkResume();
+  },
+
+  async checkResume() {
+    try {
+      const res = await request({ url: '/resume/current' });
+      if (res.code === 0) this.setData({ hasResume: true });
+    } catch (e) {
+      // 404 or other -> no resume
+      this.setData({ hasResume: false });
+    }
+  },
+
+  goForm() {
+    wx.navigateTo({ url: '/pages/form/form' });
+  },
+
+  goPreview() {
+    wx.navigateTo({ url: '/pages/preview/preview' });
+  },
+});
