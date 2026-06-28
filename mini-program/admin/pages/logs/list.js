ef99095961291949130a7c71ee2554e9861e3b30
YouWeiBot <youwei@mini-web>
2026-06-28 23:20:53 +0800

    feat(mini-program): admin logs list page

---
+++ mini-program/admin/pages/logs/list.js
@@ -0,0 +1,23 @@
+const { request } = require('../../../utils/request');
+
+Page({
+  data: { items: [], total: 0, page: 1, pageSize: 20, loading: false },
+
+  onShow() { this.load(); },
+
+  async load() {
+    this.setData({ loading: true });
+    try {
+      const res = await request({ url: `/admin/logs?page=${this.data.page}&pageSize=${this.data.pageSize}` });
+      const all = this.data.page === 1 ? res.data.items : this.data.items.concat(res.data.items);
+      this.setData({ items: all, total: res.data.total, loading: false });
+    } catch (e) { this.setData({ loading: false }); }
+  },
+
+  onReachBottom() {
+    if (this.data.items.length < this.data.total) {
+      this.setData({ page: this.data.page + 1 });
+      this.load();
+    }
+  },
+});
