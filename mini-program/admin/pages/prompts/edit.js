ef33e4209ce62789fcb2a8435d61d19d6fff5785
YouWeiBot <youwei@mini-web>
2026-06-28 23:19:56 +0800

    feat(mini-program): admin prompts list/edit pages

---
+++ mini-program/admin/pages/prompts/edit.js
@@ -0,0 +1,33 @@
+const { request } = require('../../../utils/request');
+
+Page({
+  data: { code: '', content: '', version: 0 },
+
+  onLoad(query) {
+    this.setData({ code: query.code });
+    this.load(query.code);
+  },
+
+  async load(code) {
+    try {
+      const res = await request({ url: `/admin/prompts/${code}` });
+      this.setData({ content: res.data.content, version: res.data.version });
+    } catch (e) {}
+  },
+
+  setContent(e) {
+    this.setData({ content: e.detail.value });
+  },
+
+  async save() {
+    if (!this.data.content.trim()) {
+      wx.showToast({ title: '内容不能为空', icon: 'none' });
+      return;
+    }
+    try {
+      await request({ url: `/admin/prompts/${this.data.code}`, method: 'PUT', data: { content: this.data.content } });
+      wx.showToast({ title: '保存成功' });
+      setTimeout(() => wx.navigateBack(), 1000);
+    } catch (err) {}
+  },
+});
---
+++ mini-program/admin/pages/prompts/list.js
@@ -0,0 +1,19 @@
+const { request } = require('../../../utils/request');
+
+Page({
+  data: { items: [] },
+
+  onShow() { this.load(); },
+
+  async load() {
+    try {
+      const res = await request({ url: '/admin/prompts' });
+      this.setData({ items: res.data.items });
+    } catch (e) {}
+  },
+
+  goEdit(e) {
+    const { code } = e.currentTarget.dataset;
+    wx.navigateTo({ url: `/admin/pages/prompts/edit?code=${code}` });
+  },
+});
